0b4d95293c64ad3e319c103a82e22136af94dba9
Omkar Joshi <omkar.nitin.joshi@gmail.com>
2023-11-01 20:19:21 +0530

    Create application_template.rb

---
+++ application_template.rb
@@ -0,0 +1,26 @@
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'capybara'
+  gem 'faker'
+  gem 'annotate'
+  gem 'rubocop', require: false
+  gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec', require: false
+end
+
+initializer 'generators.rb', <<-CODE
+  Rails.application.config.generators do |g|
+    g.test_framework :rspec,
+      fixtures:         false,
+      view_specs:       false,
+      helper_specs:     false,
+      routing_specs:    false,
+      request_specs:    false,
+      controller_specs: false
+  end
+CODE
+
+after_bundle do
+  generate 'rspec:install'
+end
