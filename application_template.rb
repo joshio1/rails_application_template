edcea67da54c42e061cfeebd0bef781fee1bd9aa
Omkar Joshi <omkar.nitin.joshi@gmail.com>
2024-04-05 01:56:11 +0530

    Update application_template.rb

--- application_template.rb
+++ application_template.rb
@@ -4,10 +4,6 @@ gem_group :development, :test do
   gem 'capybara'
   gem 'faker'
   gem 'annotate'
-  gem 'rubocop', require: false
-  gem 'rubocop-rails', require: false
-  gem 'rubocop-performance', require: false
-  gem 'rubocop-rspec', require: false
 end
 
 initializer 'generators.rb', <<-CODE
