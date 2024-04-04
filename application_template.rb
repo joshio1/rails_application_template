caa37b5a765ceaf589e6e2f7374440c730e4122f
Omkar Joshi <omkar.nitin.joshi@gmail.com>
2024-04-05 02:00:40 +0530

    Update application_template.rb

--- application_template.rb
+++ application_template.rb
@@ -4,7 +4,23 @@ gem_group :development, :test do
   gem 'capybara'
   gem 'faker'
   gem 'annotate'
+  gem 'rubocop', require: false
+  gem 'rubocop-rails', require: false
+  gem 'rubocop-performance', require: false
+  gem 'rubocop-rspec', require: false
 end
+
+initializer 'generators.rb', <<-CODE
+  Rails.application.config.generators do |g|
+    g.test_framework :rspec,
+      fixtures:         false,
+      view_specs:       false,
+      helper_specs:     false,
+      routing_specs:    false,
+      request_specs:    false,
+      controller_specs: false
+  end
+CODE
 
 after_bundle do
   generate 'rspec:install'
