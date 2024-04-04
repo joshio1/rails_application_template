35778054b3801ddfcf877b2bdeb4fb918df76306
Omkar Joshi <omkar.nitin.joshi@gmail.com>
2024-04-05 01:57:41 +0530

    Update application_template.rb

--- application_template.rb
+++ application_template.rb
@@ -6,18 +6,6 @@ gem_group :development, :test do
   gem 'annotate'
 end
 
-initializer 'generators.rb', <<-CODE
-  Rails.application.config.generators do |g|
-    g.test_framework :rspec,
-      fixtures:         false,
-      view_specs:       false,
-      helper_specs:     false,
-      routing_specs:    false,
-      request_specs:    false,
-      controller_specs: false
-  end
-CODE
-
 after_bundle do
   generate 'rspec:install'
 end
